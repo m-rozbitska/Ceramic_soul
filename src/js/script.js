01168de62cb699daa02b275e77b2c35766f5dcb5
Mariia Rozbitska <melissa180lily@gmail.com>
2026-08-21 23:05:55 +0200

    add desktop  responsive styles for activities  section

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
-import "purecss/build/grids-min.css";
-import "purecss/build/grids-responsive-min.css";
+/* import "purecss/build/grids-min.css";
+import "purecss/build/grids-responsive-min.css"; */
+
 
 import "/src/sass/style.scss";
 
